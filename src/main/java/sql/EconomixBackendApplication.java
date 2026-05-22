b8c94c938febcf61de3fd14e129a51da78e9b98b
Solan26007 <solanorojasgabriel@gmail.com>
2026-05-21 19:06:33 -0600

    ya quedo pero cuidado con esta version va con la de ECONOMIX_ANDRIOD que dice ES CLAVE

--- src/main/java/sql/EconomixBackendApplication.java
+++ src/main/java/sql/EconomixBackendApplication.java
@@ -2,7 +2,7 @@ package sql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//YA JALA CUATEEEEE
 @SpringBootApplication
 public class EconomixBackendApplication {
     public static void main(String[] args) {
